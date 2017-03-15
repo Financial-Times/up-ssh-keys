b918b6c02208456f3086e120b17dd744d91dc9a1
Euan.Finlay <euan.finlay@ft.com>
2017-03-15 15:59:21 +0000

    removing AD extraction script, we don't use it currently

--- extract_from_ad.sh
+++
@@ -1,1 +0,0 @@
-ldapsearch -o  ldif-wrap=no -x -vv -w$LINUX_LDAP_PASSWORD -D "CN=linuxldap,OU=Service Accounts,OU=FT Service Admins,DC=ftroot,DC=com" -H ldap://ldap.ftroot.com/ -b "DC=ftroot,DC=com" -LLL -s sub '(memberOf=CN=DLO-Universal Publishing,OU=Infrastructure Groups,OU=Groups,OU=OSB,OU=EMEA,DC=FTROOT,DC=com)' info  | grep ^info:\ ssh- | sed s/^info:\ //  >authorized_keys_ad
